6542c4dc220ce824e1a23a94752ef41cf773feba
Kllaster <noityt@mail.ru>
2021-03-14 23:06:36 +0300

    Updated the square calculation

--- render/intersections/inter_square.c
+++ render/intersections/inter_square.c
@@ -3,21 +3,24 @@
 void	inter_square(const t_square *s_square, t_ray *s_ray)
 {
 	float		x;
-	float		a;
 	t_vec		s_vec1;
+	t_matrix	s_matrix;
 
-	s_vec1 = vec_sub(&s_ray->s_vec_start, &s_square->s_vec_origin);
-	x = vec_dot(&s_vec1, &s_square->s_vec_dir);
-	a = vec_dot(&s_ray->s_vec_start_dir, &s_square->s_vec_dir);
-	if (a == 0 || (x < 0 && a < 0) || (x > 0 && a > 0) ||
-		(x = -x / a) <= MIN_DISTANCE || s_ray->length <= x)
+	if ((x = vec_dot(&s_ray->s_vec_start_dir, &s_square->s_vec_dir)) == 0)
+		return ;
+	s_vec1 = vec_sub(&s_square->s_vec_origin, &s_ray->s_vec_start);
+	x = vec_dot(&s_vec1, &s_square->s_vec_dir) / x;
+	if (x <= MIN_DISTANCE || s_ray->length <= x)
 		return ;
 	s_vec1 = vec_mul(&s_ray->s_vec_start_dir, x);
-	s_vec1 = vec_sum(&s_vec1, &s_ray->s_vec_start);
+	s_vec1 = vec_sum(&s_ray->s_vec_start, &s_vec1);
 	s_vec1 = vec_sub(&s_vec1, &s_square->s_vec_origin);
-	a = (float)(s_square->side_size * 0.5);
-	if (ft_fabs(s_vec1.x) > a ||
-			ft_fabs(s_vec1.y) > a || ft_fabs(s_vec1.z) > a)
+	s_matrix = get_matrix_rotate((t_vec *)&s_square->s_vec_origin,
+							  		(t_vec *)&s_square->s_vec_dir);
+	if (ft_fabs(vec_dot((t_vec *)&(s_matrix.el[0]), &s_vec1)) >
+			s_square->side_size * 0.5 ||
+			ft_fabs(vec_dot((t_vec *)&(s_matrix.el[1]), &s_vec1)) >
+			s_square->side_size * 0.5)
 		return ;
 	add_inter_res(s_ray, &s_square->s_material, x);
 	s_ray->last_inter_obj = (void *)s_square;
